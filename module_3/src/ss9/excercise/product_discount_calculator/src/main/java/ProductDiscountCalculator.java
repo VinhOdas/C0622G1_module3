5c77fbd7a1724e95de1871e7b93674ea4adf3f84
Your Name <you@example.com>
2022-09-26 16:50:43 +0700

    ss9

--- module_3/src/ss9/excercise/product_discount_calculator/src/main/java/ProductDiscountCalculator.java
+++ module_3/src/ss9/excercise/product_discount_calculator/src/main/java/ProductDiscountCalculator.java
@@ -13,12 +13,14 @@ public class ProductDiscountCalculator extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         String productDescription = request.getParameter("product_description");
         double listPrice = Double.parseDouble(request.getParameter("list_price"));
+        BigDecimal listPrice1 = new BigDecimal(listPrice);
         double discountPercent = Double.parseDouble((request.getParameter("discount_percent")));
+        BigDecimal discountPercent1 = new BigDecimal(discountPercent);
         BigDecimal discountAmount = new BigDecimal(listPrice * discountPercent * 0.01);
         BigDecimal discountPrice = new BigDecimal(listPrice + (listPrice * discountPercent * 0.01));
         request.setAttribute("productDescription", productDescription);
-        request.setAttribute("listPrice", listPrice);
-        request.setAttribute("discountPercent", discountPercent);
+        request.setAttribute("listPrice", listPrice1);
+        request.setAttribute("discountPercent", discountPercent1);
         request.setAttribute("discountAmount", discountAmount);
         request.setAttribute("discountPrice", discountPrice);
         request.getRequestDispatcher("/index.jsp").forward(request, response);
