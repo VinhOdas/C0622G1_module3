1480d1bc857fc9e0a12c6b63c8c912e9536cb823
Your Name <you@example.com>
2022-09-26 16:13:21 +0700

    ss9

---
+++ module_3/src/ss9/excercise/product_discount_calculator/src/main/java/ProductDiscountCalculator.java
@@ -0,0 +1,29 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.math.BigDecimal;
+
+@WebServlet(name = "ProductDiscountCalculator", urlPatterns = {"/display", "/hien-len"})
+public class ProductDiscountCalculator extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+        String productDescription = request.getParameter("product_description");
+        double listPrice = Double.parseDouble(request.getParameter("list_price"));
+        double discountPercent = Double.parseDouble((request.getParameter("discount_percent")));
+        BigDecimal discountAmount = new BigDecimal(listPrice * discountPercent * 0.01);
+        BigDecimal discountPrice = new BigDecimal(listPrice + (listPrice * discountPercent * 0.01));
+        request.setAttribute("productDescription", productDescription);
+        request.setAttribute("listPrice", listPrice);
+        request.setAttribute("discountPercent", discountPercent);
+        request.setAttribute("discountAmount", discountAmount);
+        request.setAttribute("discountPrice", discountPrice);
+        request.getRequestDispatcher("/index.jsp").forward(request, response);
+
+
+    }
+
+}
---
+++ module_3/src/ss9/excercise/product_discount_calculator/src/main/webapp/index.jsp
@@ -0,0 +1,26 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Vinho
+  Date: 9/26/2022
+  Time: 1:39 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Ứng dụng Product Discount Calculator</title>
+</head>
+<body>
+<form action="/display" method="post">
+    <input placeholder="Product Description:" type="text" name="product_description">
+    <input placeholder="List Price" type="text" name="list_price">
+    <input placeholder="Discount Percent" type="text" name="discount_percent">
+    <input type="submit" id="submit" value="sadsa">
+</form>
+<h1> Mô tả sản phẩm: ${productDescription}</h1>
+<h1> Giá niêm yết của sản phẩm: ${listPrice}</h1>
+<h1> Tỷ lệ chiết khấu (phần trăm): ${discountPercent}</h1>
+<h1> Lượng chiết khâu: ${discountAmount}</h1>
+<h1> giá sau khi chiết khấu: ${discountPrice}</h1>
+</body>
+</html>
