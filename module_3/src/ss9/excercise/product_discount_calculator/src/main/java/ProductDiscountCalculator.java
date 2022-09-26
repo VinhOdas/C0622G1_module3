7ed06d288377fcb50ffae360a9e88e0be8182283
Your Name <you@example.com>
2022-09-26 21:41:39 +0700

    fix ss9

--- module_3/src/ss9/excercise/product_discount_calculator/src/main/java/ProductDiscountCalculator.java
+++ module_3/src/ss9/excercise/product_discount_calculator/src/main/java/ProductDiscountCalculator.java
@@ -9,16 +9,20 @@ import java.math.BigDecimal;
 @WebServlet(name = "ProductDiscountCalculator", urlPatterns = {"/display", "/hien-len"})
 public class ProductDiscountCalculator extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         request.setCharacterEncoding("UTF-8");
         response.setContentType("text/html;charset=UTF-8");
+
         String productDescription = request.getParameter("product_description");
         double listPrice = Double.parseDouble(request.getParameter("list_price"));
         BigDecimal listPrice1 = new BigDecimal(listPrice);
         double discountPercent = Double.parseDouble((request.getParameter("discount_percent")));
+
         BigDecimal discountPercent1 = new BigDecimal(discountPercent);
         BigDecimal discountAmount = new BigDecimal(listPrice * discountPercent * 0.01);
         BigDecimal discountPrice = new BigDecimal(listPrice + (listPrice * discountPercent * 0.01));
         request.setAttribute("productDescription", productDescription);
+
         request.setAttribute("listPrice", listPrice1);
         request.setAttribute("discountPercent", discountPercent1);
         request.setAttribute("discountAmount", discountAmount);
--- module_3/src/ss9/excercise/product_discount_calculator/src/main/webapp/index.jsp
+++ module_3/src/ss9/excercise/product_discount_calculator/src/main/webapp/index.jsp
@@ -17,10 +17,15 @@
     <input placeholder="Discount Percent" type="text" name="discount_percent">
     <input type="submit" id="submit" value="sadsa">
 </form>
+
 <h1> Mô tả sản phẩm: ${productDescription}</h1>
+
 <h1> Giá niêm yết của sản phẩm: ${listPrice}</h1>
+
 <h1> Tỷ lệ chiết khấu (phần trăm): ${discountPercent}</h1>
+
 <h1> Lượng chiết khâu: ${discountAmount}</h1>
+
 <h1> giá sau khi chiết khấu: ${discountPrice}</h1>
 </body>
 </html>
