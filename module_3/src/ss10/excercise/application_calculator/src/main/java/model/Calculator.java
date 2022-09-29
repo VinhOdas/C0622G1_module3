ae2bc3fda4a9463f141ca13d82f2f92fc30741f1
Your Name <you@example.com>
2022-09-29 18:49:41 +0700

    ss10

--- module_3/src/ss10/excercise/application_calculator/src/main/java/model/Calculator.java
+++ module_3/src/ss10/excercise/application_calculator/src/main/java/model/Calculator.java
@@ -1,5 +1,24 @@
 package model;
 
 public class Calculator {
+    public static float caculate(float firstOperand, float secondOperand, int operator) {
+        float result = 0;
+        switch (operator) {
+            case '+':
+                return firstOperand + secondOperand;
+            case '-':
+                return firstOperand - secondOperand;
+            case '*':
+                return firstOperand * secondOperand;
+            case '/':
+                if (secondOperand != 0)
+                    return firstOperand / secondOperand;
+                else
+                    throw new RuntimeException("Can't divide by zero");
+            default:
+                throw new RuntimeException("Invalid operation");
+        }
+    }
+
 
 }
