39d766b7b064e1f4be2b4aeaf66a6342ade1b7cd
Your Name <you@example.com>
2022-10-05 18:35:50 +0700

    case

---
+++ case_study_module_3/case_study/database_case_study.sql
@@ -0,0 +1,141 @@
+DROP DATABASE IF EXISTS case_study;
+CREATE DATABASE case_study;
+USE case_study;
+CREATE TABLE position (
+    id_positio sn INT PRIMARY KEY AUTO_INCREMENT,
+    name_position VARCHAR(45)
+);
+CREATE TABLE education_degree (
+    id_education_degree INT PRIMARY KEY AUTO_INCREMENT,
+    name_education_degree VARCHAR(45)
+);
+CREATE TABLE division (
+    id_division INT PRIMARY KEY AUTO_INCREMENT,
+    name_division VARCHAR(45)
+);
+create table `user`(
+username varchar(255) primary key,
+password varchar (255)
+);
+create table `role`(
+role_id int primary key,
+role_name varchar(255)
+);
+create table user_role(
+role_id int,
+username varchar(255),
+primary key(role_id, username),
+foreign key(role_id) references `role`(role_id),
+foreign key(username) references `user`(username)
+);
+CREATE TABLE employee (
+    id_employee INT PRIMARY KEY,
+    name_employee VARCHAR(45),
+    date_of_birth_employee DATE,
+    id_card_employee VARCHAR(45),
+    salary_employee DOUBLE,
+    phone_number_employee VARCHAR(45),
+    email_employee VARCHAR(45),
+    address_employee VARCHAR(45),
+    id_position INT NOT NULL,
+    username varchar(255),
+	CONSTRAINT fk_htk_id_position
+	FOREIGN KEY (id_position)
+	REFERENCES `position`(id_position),
+    id_education_degree INT NOT NULL,
+    CONSTRAINT fk_htk_id_education_degree
+	FOREIGN KEY (id_education_degree)
+	REFERENCES education_degree(id_education_degree),
+    id_division INT NOT NULL,
+    CONSTRAINT fk_htk_id_division
+	FOREIGN KEY (id_division)
+	REFERENCES division(id_division),
+    foreign key (username)
+    references `user`(username)
+);
+CREATE TABLE customer_type(
+	id_customer_type INT PRIMARY KEY AUTO_INCREMENT,
+	name_customer_type VARCHAR(45)
+);
+CREATE TABLE facility_type(
+	id_facility_type INT PRIMARY KEY AUTO_INCREMENT,
+	name_facility_type VARCHAR(45)
+);
+CREATE TABLE rent_type(
+	id_rent_type INT PRIMARY KEY AUTO_INCREMENT,
+	name_rent_type VARCHAR(45)
+);
+CREATE TABLE customer(
+	id_customer INT PRIMARY KEY,
+	name_customer VARCHAR(45),
+	date_of_birth_customer DATE,
+	gender_customer BIT(1),
+	id_card_customer VARCHAR(45),
+	phone_number_customer VARCHAR(45),
+	email_customer VARCHAR(45),
+	address_customer VARCHAR(45),
+	id_customer_type INT NOT NULL,
+    CONSTRAINT fk_htk_id_customer_type
+	FOREIGN KEY (id_customer_type)
+	REFERENCES customer_type(id_customer_type)
+);
+
+CREATE TABLE attach_facility(
+	id_attach_facility INT PRIMARY KEY,
+	name_attach_facility VARCHAR(45),
+	cost_attach_facility DOUBLE,
+	unit_attach_facility VARCHAR(10),
+    status_attach_facility VARCHAR(45)
+);
+
+CREATE TABLE facility(
+	id_facility INT PRIMARY KEY,
+	name_facility VARCHAR(45),
+	area_facility INT,
+    cost_facility DOUBLE,
+	max_people_facility INT,
+	standard_room VARCHAR(45),
+    description_other_convenience VARCHAR(45),
+    pool_area DOUBLE,
+    number_of_floors INT,
+    facility_free TEXT,
+	id_rent_type INT NOT NULL,
+	CONSTRAINT fk_htk_id_rent_type
+	FOREIGN KEY (id_rent_type)
+	REFERENCES rent_type(id_rent_type),
+	id_facility_type INT NOT NULL,
+	CONSTRAINT fk_htk_id_facility_type
+	FOREIGN KEY (id_facility_type)
+	REFERENCES facility_type(id_facility_type)
+);
+
+CREATE TABLE contract(
+	id_contract INT PRIMARY KEY,
+	start_date DATETIME,
+	end_date DATETIME,
+	deposit DOUBLE,
+	id_employee INT NOT NULL,
+    CONSTRAINT fk_htk_id_employee
+	FOREIGN KEY (id_employee)
+	REFERENCES employee(id_employee),
+	id_customer INT NOT NULL,
+	CONSTRAINT fk_htk_id_customer 
+	FOREIGN KEY (id_customer )
+	REFERENCES customer(id_customer),
+	id_facility  INT NOT NULL,
+    CONSTRAINT fk_htk_id_facility 
+	FOREIGN KEY (id_facility)
+	REFERENCES facility(id_facility)
+);
+CREATE TABLE contract_detail(
+	id_contract_detail INT PRIMARY KEY,
+    quantity INT,
+	id_contract INT NOT NULL,
+    CONSTRAINT fk_htk_id_contract
+	FOREIGN KEY (id_contract)
+	REFERENCES contract(id_contract),
+	id_attach_facility INT NOT NULL,
+    CONSTRAINT fk_htk_id_attach_facility
+	FOREIGN KEY (id_attach_facility)
+	REFERENCES attach_facility(id_attach_facility)
+);
---
+++ case_study_module_3/case_study/src/main/java/controller/EmployeeServlet.java
@@ -0,0 +1,21 @@
+package controller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "EmployeeServlet", urlPatterns = "/employee")
+public class EmployeeServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("view/employee.jsp");
+        dispatcher.forward(request,response);
+    }
+}
